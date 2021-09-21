d8d71b33d742de1821d58aa8f1f7e56aadef6666
Joschua Schneider <joschua.schneider@atmina.de>
2021-09-21 16:53:46 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,172 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { TransformerNames } from './compute.worker'
+
+import chair from './public/chair.webp'
+import meme from './public/meme.jpg'
+import joy from './public/joy.png'
+
+const images = [chair, meme, joy]
+
+const transformations: TransformerNames[] = [
+  'grayscale',
+  'random',
+  'bayer',
+  'bayerRGB',
+  'clampGrayscale',
+  'clamp',
+  'invert',
+  'invertShifted'
+]
+
+const imageToImageData = async (imageUrl: string) => {
+  const imageElement = document.createElement('img')
+  imageElement.src = imageUrl
+
+  await new Promise((resolve, reject) => {
+    imageElement.onload = resolve
+    imageElement.onerror = reject
+  })
+
+  const canvas = document.createElement('canvas')
+
+  canvas.width = imageElement.naturalWidth
+  canvas.height = imageElement.naturalHeight
+  const context = canvas.getContext('2d')
+  context?.drawImage(imageElement, 0, 0)
+
+  return context?.getImageData(0, 0, canvas.width, canvas.height) ?? null
+}
+
+const applyImageDataToCanvas = (
+  imageData: ImageData,
+  canvas: HTMLCanvasElement
+) => {
+  canvas.width = imageData.width
+  canvas.height = imageData.height
+  const context = canvas.getContext('2d')
+  context?.putImageData(imageData, 0, 0)
+}
+
+const workers: Worker[] = transformations.map(
+  () =>
+    new Worker(new URL('./compute.worker.ts', import.meta.url), {
+      type: 'module'
+    })
+)
+
+function useDebouncedValue<T>(value: T, delay = 100) {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [value])
+
+  return debouncedValue
+}
+
+function App() {
+  const [image, setImage] = useState(images[0])
+  const [imageData, setImageData] = useState<ImageData | null>(null)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const resultRefs = useRef<(HTMLCanvasElement | null)[]>(
+    transformations.map(() => null)
+  )
+  const [threshhold, setThreshhold] = useState(150)
+  const debouncedThreshold = useDebouncedValue(threshhold)
+
+  useEffect(() => {
+    const load = async () => {
+      const imageData = await imageToImageData(image)
+
+      if (imageData && canvasRef.current) {
+        setImageData(imageData)
+      }
+    }
+    load()
+  }, [canvasRef.current, image])
+
+  useEffect(() => {
+    for (let i = 0; i < transformations.length; i++) {
+      workers[i].addEventListener('message', (message) => {
+        const resRef = resultRefs.current[i]
+        if (resRef) {
+          applyImageDataToCanvas(message.data, resRef)
+        }
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (imageData) {
+      for (let i = 0; i < transformations.length; i++) {
+        workers[i].postMessage({
+          imageData,
+          threshold: debouncedThreshold,
+          transformName: transformations[i]
+        })
+      }
+    }
+  }, [debouncedThreshold, imageData])
+
+  useEffect(() => {
+    if (canvasRef.current && imageData) {
+      applyImageDataToCanvas(imageData, canvasRef.current)
+    }
+  }, [imageData, canvasRef])
+
+  return (
+    <div className="bg-gray-50">
+      <div className="flex sticky top-0 space-x-4 bg-white/75 z-10 py-1 px-2">
+        <input
+          type="range"
+          min="0"
+          max="255"
+          value={threshhold}
+          onChange={(e) => setThreshhold(e.target.valueAsNumber)}
+        />
+        <p>Threshold: {threshhold}</p>
+        <p>
+          {(imageData?.width ?? 0) * (imageData?.height ?? 0)} Pixel (
+          {(imageData?.width ?? 0) * (imageData?.height ?? 0) * 3} RGB Values)
+        </p>
+        {images.map((image, index) => (
+          <button
+            key={index}
+            className="px-2 bg-green-100 rounded border border-gray-100"
+            onClick={() => setImage(image)}
+          >
+            img {index}
+          </button>
+        ))}
+      </div>
+      <div className="flex flex-wrap">
+        <div className="relative">
+          <canvas ref={canvasRef} />
+          <div className="absolute top-0 left-0 px-2 text-white bg-black">
+            original
+          </div>
+        </div>
+
+        {transformations.map((transformation, index) => (
+          <div key={transformation} className="relative">
+            <canvas
+              key={index}
+              ref={(element) => (resultRefs.current[index] = element)}
+            />
+            <div className="absolute top-0 left-0 px-2 text-white bg-black">
+              {transformation}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/compute.worker.ts
@@ -0,0 +1,276 @@
+const grayscaleLuminanceHelper = (
+  imageData: ImageData,
+  resultImageData: ImageData
+) => {
+  const lumR = []
+  const lumG = []
+  const lumB = []
+  for (let i = 0; i < 256; i++) {
+    lumR[i] = i * 0.299
+    lumG[i] = i * 0.587
+    lumB[i] = i * 0.114
+  }
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    resultImageData.data[i] = lumR[imageData.data[i]]
+    resultImageData.data[i + 1] = lumR[imageData.data[i]]
+    resultImageData.data[i + 2] = lumR[imageData.data[i]]
+  }
+}
+
+const bayer: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  grayscaleLuminanceHelper(imageData, resultImageData)
+
+  const bayerThresholdMap = [
+    [15, 135, 45, 165],
+    [195, 75, 225, 105],
+    [60, 180, 30, 150],
+    [240, 120, 210, 90]
+  ]
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const x = (i / 4) % imageData.width
+    const y = Math.floor(i / 4 / imageData.width)
+    const mapped = Math.floor(
+      (imageData.data[i] + bayerThresholdMap[x % 4][y % 4]) / 2
+    )
+    const currentValue = mapped < threshold ? 0 : 255
+
+    resultImageData.data[i] = currentValue
+    resultImageData.data[i + 1] = currentValue
+    resultImageData.data[i + 2] = currentValue
+  }
+
+  return resultImageData
+}
+
+const bayerRGB: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  grayscaleLuminanceHelper(imageData, resultImageData)
+
+  const bayerThresholdMap = [
+    [15, 135, 45, 165],
+    [195, 75, 225, 105],
+    [60, 180, 30, 150],
+    [240, 120, 210, 90]
+  ]
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    for (let j = 0; j < 3; j++) {
+      const x = (i / 4) % imageData.width
+      const y = Math.floor(i / 4 / imageData.width)
+      const mapped = Math.floor(
+        (imageData.data[i + j] + bayerThresholdMap[x % 4][y % 4]) / 2
+      )
+      resultImageData.data[i + j] = mapped < threshold ? 0 : 255
+    }
+  }
+
+  return resultImageData
+}
+
+const clampGrayscale: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  const clampValues = [0, 50, 100, 150, 200, 255]
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const currentValue = clampValues.reduce((a, b) => {
+      return Math.abs(b - imageData.data[i]) < Math.abs(a - imageData.data[i])
+        ? b
+        : a
+    }, 0)
+
+    resultImageData.data[i] = currentValue
+    resultImageData.data[i + 1] = currentValue
+    resultImageData.data[i + 2] = currentValue
+  }
+
+  return resultImageData
+}
+
+const clamp: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  const clampValues = [0, 50, 100, 150, 200, 255]
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const currentValueR = clampValues.reduce((a, b) => {
+      return Math.abs(b - imageData.data[i]) < Math.abs(a - imageData.data[i])
+        ? b
+        : a
+    }, 0)
+    const currentValueG = clampValues.reduce((a, b) => {
+      return Math.abs(b - imageData.data[i + 1]) <
+        Math.abs(a - imageData.data[i + 1])
+        ? b
+        : a
+    }, 0)
+    const currentValueB = clampValues.reduce((a, b) => {
+      return Math.abs(b - imageData.data[i + 2]) <
+        Math.abs(a - imageData.data[i + 2])
+        ? b
+        : a
+    }, 0)
+
+    resultImageData.data[i] = currentValueR
+    resultImageData.data[i + 1] = currentValueG
+    resultImageData.data[i + 2] = currentValueB
+  }
+
+  return resultImageData
+}
+
+const invert: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const currentValueR = 256 - imageData.data[i]
+    const currentValueG = 256 - imageData.data[i + 1]
+    const currentValueB = 256 - imageData.data[i + 2]
+
+    resultImageData.data[i] = currentValueR
+    resultImageData.data[i + 1] = currentValueG
+    resultImageData.data[i + 2] = currentValueB
+  }
+
+  return resultImageData
+}
+
+const invertShifted: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const currentValueR = 256 - imageData.data[i + 2]
+    const currentValueG = 256 - imageData.data[i]
+    const currentValueB = 256 - imageData.data[i + 1]
+
+    resultImageData.data[i] = currentValueR
+    resultImageData.data[i + 1] = currentValueG
+    resultImageData.data[i + 2] = currentValueB
+  }
+
+  return resultImageData
+}
+
+const grayscale: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  grayscaleLuminanceHelper(imageData, resultImageData)
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const currentValue = imageData.data[i] > threshold ? 256 : 0
+
+    resultImageData.data[i] = currentValue
+    resultImageData.data[i + 1] = currentValue
+    resultImageData.data[i + 2] = currentValue
+  }
+
+  return resultImageData
+}
+
+const random: Transformer = async (
+  imageData: ImageData,
+  threshold: number
+): Promise<ImageData> => {
+  const resultImageData = new ImageData(
+    Uint8ClampedArray.from(imageData.data),
+    imageData.width,
+    imageData.height
+  )
+
+  grayscaleLuminanceHelper(imageData, resultImageData)
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    const currentValue = imageData.data[i] > Math.random() * threshold ? 256 : 0
+
+    resultImageData.data[i] = currentValue
+    resultImageData.data[i + 1] = currentValue
+    resultImageData.data[i + 2] = currentValue
+  }
+
+  return resultImageData
+}
+
+const transformations = {
+  bayer,
+  bayerRGB,
+  clampGrayscale,
+  clamp,
+  grayscale,
+  random,
+  invert,
+  invertShifted
+}
+
+self.addEventListener('message', (message: MessageEvent<WorkerMessage>) => {
+  const transformer = transformations[message.data.transformName]
+  transformer(message.data.imageData, message.data.threshold).then((result) => {
+    postMessage(result)
+  })
+})
+
+export type Transformer = (
+  imageData: ImageData,
+  threshold: number
+) => Promise<ImageData>
+
+export type TransformerNames = keyof typeof transformations
+
+export type WorkerMessage = {
+  imageData: ImageData
+  transformName: keyof typeof transformations
+  threshold: number
+}
